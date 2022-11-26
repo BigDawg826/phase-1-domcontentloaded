464bf42f8ee3f71a84a84ca43a1a4b54806005a3
Darian Fultz <dfultz826@yahoo.com>
2022-11-26 16:42:19 -0500

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').textContent = "This is really cool!";
+}
+)
+
+// Solutions given
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     updateDOM()
+// });
+// function updateDOM(){
+//     document.getElementById('text' ).innerHTML = "This is really cool!";
+// }
+// or 
+
+// function DOMContentLoaded() {
+//     document.addEventListener('DOMContentLoaded', function(event){
+//         updateDOM()
+//     })
+// }
+// function updateDOM() {
+//     document.getElementById('text').innerHTML = "This is really cool!";
+// }
